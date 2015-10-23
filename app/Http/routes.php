41f2c7e49fe9b10887ad4f83cd22f5eb228bec85
AnanasKelly <anastasiya_johnni@mail.ru>
2015-10-23 17:04:16 +0300

    add new route,controller,views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,28 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/', function(){
 
-Route::get('/', function () {
     return view('welcome');
 });
+Route::get('Ceacat',[
+    'as' => 'ceacat.index', 'uses' => 'Ceacat@index'
+]);
+Route::get('Ceacat/create',[
+    'as' => 'ceacat.create', 'uses' => 'Ceacat@create'
+]);
+Route::post('Ceacat',[
+    'as' => 'ceacat.store', 'uses' => 'Ceacat@store'
+]);
+Route::get('Ceacat/{id}',[
+    'as' => 'ceacat.show', 'uses' => 'Ceacat@show'
+]);
+Route::get('Ceacat/{id}/edit',[
+    'as' => 'ceacat.edit', 'uses' => 'Ceacat@edit'
+]);
+Route::put('Ceacat/{id}',[
+    'as' => 'ceacat.update', 'uses' => 'Ceacat@edit'
+]);
+Route::delete('Ceacat/{id}',[
+    'as' => 'ceacat.destroy' , 'uses' => 'Ceacat@destroy'
+]);
