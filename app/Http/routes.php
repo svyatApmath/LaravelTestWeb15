f234df79ab87861cffe0d6cb99f551ee5f5fb7d5
Iren <nor.iren@yandex.ru>
2015-10-24 00:14:24 +0300

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,7 +17,7 @@ Route::get('countries/create',[
     'as' => 'create',
     'uses' => 'CountriesController@create'
     ]);
-Route::get('countries/1',[
+Route::get('countries/{id}',[
     'as' => 'show',
     'uses' => 'CountriesController@show'
     ]);
@@ -38,15 +28,15 @@ Route::post('countries/',[
     ]);
  
  
-Route::get('countries/1/edit',[
+Route::get('countries/{id}/edit',[
     'as' => 'edit',
     'uses' => 'CountriesController@edit'
     ]);
-Route::post('countries/1',[
+Route::post('countries/{id}',[
     'as' => 'update',
     'uses' => 'CountriesController@update'
     ]);
-Route::delete('countries/1',[
+Route::delete('countries/{id}',[
     'as' => 'destroy',
     'uses' => 'CountriesController@destroy'
     ]);
