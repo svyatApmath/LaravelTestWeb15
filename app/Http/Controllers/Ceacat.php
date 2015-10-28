53d5182f02b0e7b62639c38cbc8bb9bcea997be4
AnanasKelly <anastasiya_johnni@mail.ru>
2015-10-29 01:27:38 +0300

    update Controller

--- app/Http/Controllers/Ceacat.php
+++ app/Http/Controllers/Ceacat.php
@@ -1,15 +1,13 @@
-<?php namespace App\Http\Controllers;
+<?php 
+
+namespace App\Http\Controllers;
+
 use App\Whale;
 use Request;
 use Illuminate\Support\Facades\Input;
+
 class Ceacat extends Controller {
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-    
 public function index()
 {
     $whales = Whale::all();
@@ -23,40 +21,42 @@ public function create()
 public function store()
 {
     $data = Request::only('id','firstName','lastName','sex');
-    $whale = new Whale();
-    $whale->identity = $data['id'];
-    $whale->firstName = $data['firstName'];
-    $whale->lastName = $data['lastName'];
-    $whale->Sex = $data['sex'];
-    $whale->save();
+    $object = new Whale();
+    $object->identity = $data['id'];
+    $object->firstname = $data['firstName'];
+    $object->lastname = $data['lastName'];
+    $object->sex = $data['sex'];
+    $object->save();
     
     return redirect('Ceacat');
 }
 public function show($id)
 {
-    $object = Whale::find(1);
+    $object = Whale::find($id);
    
-    return $object;
+    return view('ceacat.show')->with('object',$object);
 }
 public function edit($id)
 {
     return view('ceacat.edit')->with('whale',Whale::find($id));
 }
-public function update()
+public function update($id)
 {
-    $new = Request::only('id','firstName','lastName','sex');
-    $old = Whale::find($new['id']);
-    $old->identity = $new->identity;
-    $old->firstName = $new->firstName;
-    $old->lastName = $new->lastName;
-    $old->Sex = $new->Sex; 
+    $new = Request::only('identity','firstname','lastname','sex');
+    $old = Whale::find($id);
+    $old->identity = $new['identity'];
+    $old->firstname = $new['firstname'];
+    $old->lastname = $new['lastname'];
+    $old->sex = $new['sex']; 
     $old->save();
+    
     return redirect('Ceacat');
 }
 public function destroy($id)
 {
-    $whale = Whale::find($id);
-    $whale->delete();
-    return $whale;
+    $object = Whale::find($id);
+    $object->delete();
+    
+    return redirect('Ceacat');
 }
 }
