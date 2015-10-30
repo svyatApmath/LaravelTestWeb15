3f8077542c32a757b62960ae9b325e83c04e7e57
artesby <artesby1@gmail.com>
2015-10-30 03:36:47 +0300

    Make Model #3.1

--- resources/views/Ceacat/Page1.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.master')
-@section('title','Some page')
-@section ('content')
-<h1>MAHALO</h1>
-{!! Form::open(['route' => 'ceacat.index','method' => 'get'])!!}
-{!!Form::submit('Back to Index', ['class' => 'btn btn-primary']) !!}
-{!!Form::close()!!}
-@stop
